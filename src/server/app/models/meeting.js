baba3181e3ff9c211581ef52b45b999a1006e02d
JuiMin <d95wang@uw.edu>
2017-05-17 12:07:52 -0700

    add the starting date for all meetings and the ending date for all meetings

--- src/server/app/models/meeting.js
+++ src/server/app/models/meeting.js
@@ -2,16 +2,20 @@ module.exports = function (sequelize, DataTypes) {
 
   var Meeting = sequelize.define('Meeting', {
     title: { type: DataTypes.STRING, allowNull: false },
-    closeoutTime: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
+    closeoutDate: { type: DataTypes.DATE, allowNull: false},
+    closeoutTime: { type: DataTypes.TIME, allowNull: false},
+    meetingsStartDate: { type: DataTypes.TIME, allowNull: false},
+    meetingsEndDate: { type: DataTypes.TIME, allowNull: false},
     generalLocation: { type: DataTypes.STRING, defaultValue: "", allowNull: false }, //  general location
     latitude: { type: DataTypes.INTEGER, defaultValue: null, validate: { min: -90, max: 90 }}, // Location latitude
     longitude: { type: DataTypes.INTEGER, defaultValue: null, validate: { min: -180, max: 180 }}, // Location longitude
     radius: { type: DataTypes.INTEGER, defaultValue: 1000, allowNull: false }, // Location Search Radius in meters
     duration: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 60 },
-    participants: { type: DataTypes.JSON, defaultValue: "", allowNull: false } // Default value is one hour -- units in minutes
+    participants: { type: DataTypes.JSON, defaultValue: "", allowNull: false },
+    creator: { type: DataTypes.STRING, defaultValue: "", allowNull: false } // Default value is one hour -- units in minutes
   }, {
     classMethods: {
-      associate: function (models) {
+        associate: function (models) {
         Meeting.hasOne(models.Decision);
         Meeting.hasMany(models.Response);
       }
