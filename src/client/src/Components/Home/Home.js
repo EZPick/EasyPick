d40405cb646f6af20510cabc3dc9e0fd1c1d94eb
Yvonne Ingles <ydingles@gmail.com>
2017-05-17 11:56:22 -0700

    test home push

--- src/client/src/Components/Home/Home.js
+++ src/client/src/Components/Home/Home.js
@@ -5,8 +5,10 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Home</h1>
-        <div>This is a test.</div>
+        <h1>Welcome to Easy Pick</h1>
+        <div>Welcome to EZ Pick! This app will help you plan where and when to meet up with friends or classmates. You’ll set up a meeting, send out a URL, and everyone can fill out what times work for them and what type of place they want to meet. You’ll get your meeting scheduled for you, automatically!
+
+To get started, click the “Create” button below.</div>
       </div>
     );
   }
