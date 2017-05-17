97b77cbf168087674d9d0e9065bcb6aecce7e972
JuiMin <d95wang@uw.edu>
2017-05-17 11:46:35 -0700

    change the data types to json for the schedule and the location preferences

--- src/server/app/models/meeting.js
+++ src/server/app/models/meeting.js
@@ -7,7 +7,7 @@ module.exports = function (sequelize, DataTypes) {
     latitude: { type: DataTypes.INTEGER, defaultValue: null, validate: { min: -90, max: 90 }}, // Location latitude
     longitude: { type: DataTypes.INTEGER, defaultValue: null, validate: { min: -180, max: 180 }}, // Location longitude
     radius: { type: DataTypes.INTEGER, defaultValue: 1000, allowNull: false }, // Location Search Radius in meters
-    duration: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 60 }
+    duration: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 60 } // Default value is one hour -- units in minutes
   }, {
     classMethods: {
       associate: function (models) {
