06a5e8a720b970a295bf63c4b71671b1f3cc8868
JuiMin <d95wang@uw.edu>
2017-05-17 11:05:32 -0700

    Fixed the meeting and response models association issue

--- src/server/app/models/response.js
+++ src/server/app/models/response.js
@@ -1,10 +1,10 @@
 module.exports = function (sequelize, DataTypes) {
 
   var Response = sequelize.define('Response', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    schedule: DataTypes.TEXT, // Stored as JSON?
-    locationPreferences: DataTypes.TEXT // Stored as JSON?
+    name: { type: DataTypes.STRING, allowNull: false }, // Responder Name
+    email: { type: DataTypes.STRING, allowNull: false, }, // Responder Email
+    schedule: { type: DataTypes.TEXT, allowNull: false }, // Stored as JSON?
+    locationPreferences: { type: DataTypes.TEXT, allowNull: false } // Stored as JSOn
   }, {
     classMethods: {
       associate: function (models) {
