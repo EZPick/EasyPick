d1411b3ab57eb1cb951d5a6064b89a4706ab9d78
Zeb Burke-Conte <zmbc@uw.edu>
2017-05-19 10:08:12 -0700

    Test dev and production routes

--- server/test/app/controllers/meeting.js
+++ server/test/app/controllers/meeting.js
@@ -1,11 +1,80 @@
 'use strict';
 
-var expect = require('chai').expect;
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+var expect = chai.expect;
+
+var express = require('express');
 
 var meeting = require('../../../app/controllers/meeting');
 
-describe('meeting routes', function() {
+describe('meeting controller', function() {
   it('should load', function() {
     expect(meeting).to.be.a('function');
+  });
+
+  describe('dev routes', function() {
+    var app;
+
+    beforeEach(function() {
+      app = express();
+      process.env.NODE_ENV = 'development';
+      meeting(app);
+    });
+
+    it('should respond to /api/meeting/create', function(done) {
+      chai.request(app)
+        .post('/api/meeting/create')
+        // Pass parameters here
+        .end(function(err, res) {
+          expect(err).to.not.exist;
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+
+    it('should respond to /api/meeting/1', function(done) {
+      chai.request(app)
+        .get('/api/meeting/1')
+        // Pass parameters here
+        .end(function(err, res) {
+          expect(err).to.not.exist;
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('production routes', function() {
+    var app;
+
+    beforeEach(function() {
+      app = express();
+      process.env.NODE_ENV = 'production';
+      meeting(app);
+    });
+
+    it('should respond to /meeting/create', function(done) {
+      chai.request(app)
+        .post('/meeting/create')
+        // Pass parameters here
+        .end(function(err, res) {
+          expect(err).to.not.exist;
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+
+    it('should respond to /meeting/1', function(done) {
+      chai.request(app)
+        .get('/meeting/1')
+        // Pass parameters here
+        .end(function(err, res) {
+          expect(err).to.not.exist;
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
   });
 });
