2241c3eb8bac8b57b94ae9c1b683330a655a24fa
Derek Wang <JuiMin@users.noreply.github.com>
2017-05-21 13:59:28 -0700

    update meeting js.
    Clearer naming on the location latitude and longitude. Remove general meeting location string

--- src/server/app/models/meeting.js
+++ src/server/app/models/meeting.js
@@ -3,9 +3,8 @@ module.exports = function (sequelize, DataTypes) {
   var Meeting = sequelize.define('Meeting', {
     title: { type: DataTypes.STRING, allowNull: false },
     closeoutTime: { type: DataTypes.DATE, allowNull: false},
-    generalLocation: { type: DataTypes.STRING, defaultValue: "", allowNull: false }, //  general location
-    latitude: { type: DataTypes.DOUBLE, defaultValue: null, validate: { min: -90, max: 90 }}, // Location latitude - general meeting area
-    longitude: { type: DataTypes.DOUBLE, defaultValue: null, validate: { min: -180, max: 180 }}, // Location longitude - general meeting area
+    generalLocationLatitude: { type: DataTypes.DOUBLE, defaultValue: null, validate: { min: -90, max: 90 }}, // Location latitude - general meeting area
+    generalLocationLongitude: { type: DataTypes.DOUBLE, defaultValue: null, validate: { min: -180, max: 180 }}, // Location longitude - general meeting area
     radius: { type: DataTypes.INTEGER, defaultValue: 1000, allowNull: false }, // Location Search Radius in meters
     duration: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 60 }, // Meeting durating - units in minutes -- default 1 hour
     invited: { type: DataTypes.JSON, defaultValue: "", allowNull: false }, // List of invited individuals (emails)
