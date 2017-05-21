a46719a52e7f3386ade0658b00283b993789a4bf
Derek Wang <JuiMin@users.noreply.github.com>
2017-05-21 13:19:50 -0700

    Update meeting
    Participants changed to 'invited' and the meeting start and end dates removed

--- src/server/app/models/meeting.js
+++ src/server/app/models/meeting.js
@@ -4,14 +4,12 @@ module.exports = function (sequelize, DataTypes) {
     title: { type: DataTypes.STRING, allowNull: false },
     closeoutDate: { type: DataTypes.DATE, allowNull: false},
     closeoutTime: { type: DataTypes.TIME, allowNull: false},
-    meetingsStartDate: { type: DataTypes.TIME, allowNull: false},
-    meetingsEndDate: { type: DataTypes.TIME, allowNull: false},
     generalLocation: { type: DataTypes.STRING, defaultValue: "", allowNull: false }, //  general location
     latitude: { type: DataTypes.INTEGER, defaultValue: null, validate: { min: -90, max: 90 }}, // Location latitude
     longitude: { type: DataTypes.INTEGER, defaultValue: null, validate: { min: -180, max: 180 }}, // Location longitude
     radius: { type: DataTypes.INTEGER, defaultValue: 1000, allowNull: false }, // Location Search Radius in meters
     duration: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 60 },
-    participants: { type: DataTypes.JSON, defaultValue: "", allowNull: false },
+    invited: { type: DataTypes.JSON, defaultValue: "", allowNull: false },
     creator: { type: DataTypes.STRING, defaultValue: "", allowNull: false } // Default value is one hour -- units in minutes
   }, {
     classMethods: {
