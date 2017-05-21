54889e8d08e49b651798d6eceb10eb84dffd0bb8
Derek Wang <JuiMin@users.noreply.github.com>
2017-05-21 13:33:59 -0700

    Updated meeting model
    The comment pertaining to meeting duration moved to the correct line

--- src/server/app/models/meeting.js
+++ src/server/app/models/meeting.js
@@ -7,9 +7,9 @@ module.exports = function (sequelize, DataTypes) {
     latitude: { type: DataTypes.INTEGER, defaultValue: null, validate: { min: -90, max: 90 }}, // Location latitude
     longitude: { type: DataTypes.INTEGER, defaultValue: null, validate: { min: -180, max: 180 }}, // Location longitude
     radius: { type: DataTypes.INTEGER, defaultValue: 1000, allowNull: false }, // Location Search Radius in meters
-    duration: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 60 },
-    invited: { type: DataTypes.JSON, defaultValue: "", allowNull: false },
-    creator: { type: DataTypes.STRING, defaultValue: "", allowNull: false } // Default value is one hour -- units in minutes
+    duration: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 60 }, // Meeting durating - units in minutes -- default 1 hour
+    invited: { type: DataTypes.JSON, defaultValue: "", allowNull: false }, // List of invited individuals (emails)
+    creator: { type: DataTypes.STRING, defaultValue: "", allowNull: false }
   }, {
     classMethods: {
       associate: function (models) {
