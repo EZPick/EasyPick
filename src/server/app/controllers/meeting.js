a24c931c6c0e54b368d8177dfdbf0eb8a2437cf3
George Ng <gngcp@uw.edu>
2017-05-22 11:01:38 -0700

    meeting controller completed but not tested, saving work

--- src/server/app/controllers/meeting.js
+++ src/server/app/controllers/meeting.js
@@ -6,17 +6,49 @@ module.exports = function (app) {
   app.use('/api/meeting/', router);
 };
 
-router.get('/:id', function (req, res, next) {
-  res.json({
-    id: req.params.id,
-    title: 'Fake Meeting'
+router.get('/get', function (req, res, next) {
+  db.Meeting.findOne({
+    where: {
+	  	id: req.params.id,
+		}
+  }).then(function(meeting) {
+    res.json({
+	  	success : true,
+	  	data : meeting.dataValues,
+		});
+  }).catch(function(err) {
+    res.json({
+	  	success : false,
+	    error : err,
+		});
   });
 });
 
 router.post('/create', function(req, res, next) {
   // Accept args in the form body
-  res.json({
-    success: true,
-    meetingId: 'this is not real'
+  db.Meeting.create({
+  // set Model values	
+
+  	title : req.title,
+		closeoutTime : req.closeoutTime,
+		generalLocationLatitude : req.generalLocationLatitude,
+		generalLocationLongitude : req.generalLocationLongitude,
+		radius : req.radius,
+		duration : req.duration,
+		invited : req.invited,
+		creator : req.creator,
+	
+  }).then(function(result) {
+    res.json({
+	  	success : true,
+	  	data : result.dataValues,
+		});
+  }).catch(function(err) {
+    res.json({
+	  	success : false,
+	  	error : err,
+		});
   });
+
 });
+
