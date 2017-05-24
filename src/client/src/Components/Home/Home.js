8ec4c0e6742612a6e5a55009cea945bf933fcc7e
Yvonne Ingles <ydingles@gmail.com>
2017-05-24 12:07:02 -0700

    home page

--- src/client/src/Components/Home/Home.js
+++ src/client/src/Components/Home/Home.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
 import './Home.css';
 
+
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to Easy Pick</h1>
-        <div>Welcome to EZ Pick! This app will help you plan where and when to meet up with friends or classmates. You’ll set up a meeting, send out a URL, and everyone can fill out what times work for them and what type of place they want to meet. You’ll get your meeting scheduled for you, automatically!
 
-To get started, click the “Create” button below.</div>
+      <div>
+      <div className="jumbotron">
+        <h1>Welcome!</h1>
+        <p className="lead">Welcome to EasyPick! This app will help you plan where and when to meet up with friends or classmates. You’ll set up a meeting, send out a URL, and everyone can fill out what times work for them and what type of place they want to meet. You’ll get your meeting scheduled for you, automatically!
+        </p>
+        
+        <p>To get started, click “Create” below.</p>
+
+        <p><a className="btn btn-lg btn-success" href="#" role="button">Create</a></p>
+              
       </div>
+
+      {/*<div className="row marketing">
+        <div className="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+
+        <div className="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+      </div>*/}
+
+      </div>
+
+
+
+
+
+
     );
   }
 }
