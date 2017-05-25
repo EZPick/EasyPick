a01e0daa41bb06bd9baefb13d684c4d2c59615f6
Yvonne Ingles <ydingles@gmail.com>
2017-05-25 11:03:08 -0700

    create form

--- src/client/src/Components/Create/Create.js
+++ src/client/src/Components/Create/Create.js
@@ -6,7 +6,34 @@ class Create extends Component {
     return (
       <div>
         <h1>Create</h1>
-        <div>This is a test.</div>
+
+        {/*begin form code*/}
+        <form id="response-form" method="post" action="/api/response/create">
+          <div>
+            <label>Name</label>
+            <input name="name" type="text" />
+          </div>
+          <p></p>
+          <div>
+            <label>Email</label>
+            <input name="email" type="text" />
+          </div>
+
+          <div>
+            <label>Location Preferences</label>
+            <input name="locationPreferences" type="text" />
+          </div>
+
+          <div>
+            <label>Schedule</label>
+            <input name="schedule" type="text" />
+          </div>
+
+          <div>
+            <input type="submit" value="Submit" />
+          </div>
+
+        </form>
       </div>
     );
   }
