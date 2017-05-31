0f03d40413ab5af8ac3902c2a9bef06142e38f03
Yvonne Ingles <ydingles@gmail.com>
2017-05-30 19:22:14 -0700

    create code

--- src/client/src/Components/Create/Create.js
+++ src/client/src/Components/Create/Create.js
@@ -1,39 +1,68 @@
 import React, { Component } from 'react';
 import './Create.css';
+import Schedule from '../Schedule/Schedule';
+import Location from '../Location/Location';
+//import $ from 'jquery';
 
 class Create extends Component {
   render() {
     return (
       <div>
-        <h1>Create</h1>
-
-        {/*begin form code*/}
-        <form id="response-form" method="post" action="/api/response/create">
-          <div>
-            <label>Name</label>
-            <input name="name" type="text" />
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-3"></div>
+            <div className="col-sm-6">
+              <h1 id="title">Create an Event</h1>
+            </div>
+            <div className="col-sm-3"></div>
           </div>
-          <p></p>
-          <div>
-            <label>Email</label>
-            <input name="email" type="text" />
+          <div className="row" id="response-row">
+            <div className="col-sm-3"></div>
+            <div className="col-sm-6">
+              <form id="response-form" onSubmit={this.submit}>
+                <input type="hidden" value={this.props.match.params.id} name="meetingId" />
+                <div className="form-group">
+                  <label>Name</label>
+                  <input name="name" className="form-control" type="text" placeholder="Type your name" required />
+                </div>
+                <div className="form-group">
+                  <label>Email</label>
+                  <input name="email" className="form-control" type="text" placeholder="Type your email" required />
+                </div>
+                <div className="form-group">
+                  <Location />
+                </div>
+                <div className="form-group">
+                  <Schedule />
+                </div>
+                <div className="form-group" id="btn-container">
+                  <button type="submit" className="btn btn-primary" id="submit-btn">
+                    Submit response
+                            </button>
+                </div>
+              </form>
+            </div>
+            <div className="col-sm-3"></div>
           </div>
-
-          <div>
-            <label>Location Preferences</label>
-            <input name="locationPreferences" type="text" />
+          <div className="row" id="confirmation-row">
+            <div className="col-sm-3"></div>
+            <div className="col-sm-6">
+              <div className="alert alert-success fade in">
+                <strong>Submission complete!</strong>
+              </div>
+            </div>
+            <div className="col-sm-3"></div>
           </div>
-
-          <div>
-            <label>Schedule</label>
-            <input name="schedule" type="text" />
+          <div className="row" id="error-row">
+            <div className="col-sm-3"></div>
+            <div className="col-sm-6">
+              <div className="alert alert-danger fade in">
+                <strong>Submission failed!</strong> Please try again
+                    </div>
+            </div>
+            <div className="col-sm-3"></div>
           </div>
-
-          <div>
-            <input type="submit" value="Submit" />
-          </div>
-
-        </form>
+        </div>
       </div>
     );
   }
