8d6646118b842cb44434f8c628c531605c284f84
JuiMin <d95wang@uw.edu>
2017-06-02 02:00:13 -0700

    removed app routing to location subview

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  //Link
 } from 'react-router-dom';
 import Home from '../Home/Home';
 import Create from '../Create/Create';
 import Respond from '../Respond/Respond';
 import Meeting from '../Meeting/Meeting';
-import Location from '../Location/Location';
 import './App.css';
 
 class App extends Component {
@@ -26,7 +24,6 @@ class App extends Component {
           <Route exact path="/create" component={Create}/>
           <Route path="/respond/:id" component={Respond}/>
           <Route path="/meeting/:id" component={Meeting}/>
-          <Route path="/location" component={Location}/>
         </div>
       </Router>
     );
