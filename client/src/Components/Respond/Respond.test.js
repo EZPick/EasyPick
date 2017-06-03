4c28133fd1dc444d3decdfd1fe8295e9006f0ba9
Zeb Burke-Conte <zmbc@uw.edu>
2017-06-03 00:16:55 -0700

    Run respond test in a MemoryRouter

--- client/src/Components/Respond/Respond.test.js
+++ client/src/Components/Respond/Respond.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter, Route } from 'react-router';
 import Respond from './Respond';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Respond />, div);
+  ReactDOM.render(
+    (<MemoryRouter initialEntries={[ '/respond/1' ]}>
+      <Route path="respond/:id" component={Respond} />
+    </MemoryRouter>),
+  div);
 });
