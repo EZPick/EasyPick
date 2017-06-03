586d68648d17d84252de302adb36c7c4c88979d6
JuiMin <d95wang@uw.edu>
2017-06-03 14:48:22 -0700

    made set state one line and fixed the state toggles

--- client/src/Components/Location/Location.js
+++ client/src/Components/Location/Location.js
@@ -5,20 +5,18 @@ class Location extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {wifi: '', privacy: '', quiet: '', foodAndDrink: ''};
+    this.state = {wifi: false, privacy: false, quiet: false, foodAndDrink: false};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({wifi: event.target.wifi});
-    this.setState({privacy: event.target.privacy});
-    this.setState({quiet: event.target.quiet});
-    this.setState({foodAndDrink: event.target.foodAndDrink});
+    this.setState({
+      [event.target.id]: !this.state[event.target.id]
+    });
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.coffee);
     event.preventDefault();
   }
 
@@ -27,28 +25,21 @@ class Location extends Component {
       <div ref="outputDiv">
         <form ref="form" onSubmit={this.handleSubmit}>
           <label className="item">
-            <input id="wifi" type="checkbox" ref="wifi" value={this.state.wifi} onChange={this.handleChange} /> Wifi
+            <input id="wifi" type="checkbox" onChange={this.handleChange} /> Wifi
           </label>
-
           <label className="item">
-            <input id="privacy" type="checkbox" ref="privacy" value={this.state.privacy} onChange={this.handleChange} /> Privacy
+            <input id="privacy" type="checkbox" onChange={this.handleChange} /> Privacy
           </label>
-
           <label className="item">
-            <input id="quiet" type="checkbox" ref="quiet" value={this.state.quiet} onChange={this.handleChange} /> Quiet
+            <input id="quiet" type="checkbox" onChange={this.handleChange} /> Quiet
           </label>
-
           <label className="item">
-            <input id="foodAndDrink" type="checkbox" ref="foodAndDrink" value={this.state.foodAndDrink} onChange={this.handleChange} /> Food and Drink
+            <input id="foodAndDrink" type="checkbox" onChange={this.handleChange} /> Food and Drink
           </label>
           <input type="submit" value="Submit" />
         </form>
       </div>
     );
-  }
-
-  value() {
-    // get the value
   }
 }
 
